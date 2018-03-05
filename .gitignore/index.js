49c35c6d51ad6e5183d7a3b6bbd5f45c202bdb49
Jordan <37051023+JojoLave13@users.noreply.github.com>
2018-03-05 13:25:18 +0100

    Update and rename .gitignore/index.js to  .gitignore/index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,3 +8,9 @@ bot.on("ready", function() {
 });
 
 bot.login("NDE5ODgyMzczNjEwNDcxNDI2.DX3rzg.DRQ4-yfJqBX2NJDa24lsQNJzAN4");
+
+bot.on('guildMemberAdd', function (member) {
+member.createDM().then(function (channel) {
+return channel.send('Bienvenue sur le server "Jordan community"' + member.displayName)
+}).catch(console.error)
+})
